cb75b09b6bfb63b0895cc71eed9546eccac3029d
Alejandro Maticorena <alejexxx123@gmail.com>
2018-06-01 12:45:57 -0500

    controlador movimiento

---
+++ Assets/scripts/controlador/move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        movPJ();
+	}
+
+    void movPJ()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, 0, -x);
+        transform.Translate(0, z, 0);
+    }
+}
