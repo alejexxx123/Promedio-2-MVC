0ebc89592d7dc4f2dae4dc15894966beca68ab52
Alfsaram <varko2-34@hotmail.com>
2018-06-01 13:20:20 -0500

    Base de la Data de puntos

---
+++ Assets/scripts/Modelo/Puntos.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puntos : MonoBehaviour {
+
+    float puntos;
+
+    // Use this for initialization
+    void Start() {
+
+        puntos = 0;
+
+	}
+
+}
